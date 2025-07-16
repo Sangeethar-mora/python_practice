c9293b882e4b941597c807ec740d44e2f7067e36
sangeetha <sangeethasrumail.com>
2025-07-16 13:12:39 +0100

    updated

--- Basic_exercise_py/Basic_ex7.py
+++ Basic_exercise_py/Basic_ex7.py
@@ -49,3 +49,55 @@ for i in max_list:
         if i == val:
             max_list.remove(i)
             print(key,"appear",val,"times")
+
+################## using functions #################
+
+def extract_words(text):
+    wrd = ""
+    wrd_list = []
+
+    for char in text:
+        if char not in [" ", "."]:
+            wrd += char
+        elif char == " " and wrd:
+            wrd_list.append(wrd.lower())
+            wrd = ""
+    if wrd:
+        wrd_list.append(wrd.lower())
+
+    return wrd_list
+
+
+def count_words(word_list):
+    count_wrd = {}
+    for word in word_list:
+        if word in count_wrd:
+            count_wrd[word] += 1
+        else:
+            count_wrd[word] = 1
+    return count_wrd
+
+
+def print_by_max_list(count_wrd):
+    max_list = []
+
+    for val in count_wrd.values():
+        max_list.append(val)
+
+    max_list = sorted(max_list, reverse=True)
+    print(max_list)
+
+    for i in max_list:
+        for key, val in count_wrd.items():
+            if i == val:
+                max_list.remove(i)
+                print(key, "appear", val, "times")
+
+def process_text(text):
+    words = extract_words(text)
+    counts = count_words(words)
+    print_by_max_list(counts)
+
+text = "Emma is good developer. Emma is a writer"
+process_text(text)
+
